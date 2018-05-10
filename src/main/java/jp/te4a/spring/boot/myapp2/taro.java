3bb39cee4c8629c33af7be863b032e0f7eb14c17
高畑 祐輝 <150161@jc-21.jp>
2018-05-10 09:57:06 +0900

    コミットメッセージ

---
+++ src/main/java/jp/te4a/spring/boot/myapp2/taro.java
@@ -0,0 +1,17 @@
+package jp.te4a.spring.boot.myapp2;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+public class taro
+{
+	@RequestMapping("/taro")
+	public String index() {
+		return "Taro desu!";
+	}
+}
